0c19981666b2d87f6c22a89ef2243b11b2c8edc5
revesansparole <revesansparole@gmail.com>
2015-12-08 17:10:52 +0100

    better deploy venv

--- script/deploy_oas_venv.py
+++ script/deploy_oas_venv.py
@@ -1,19 +1,21 @@
 from os import chdir, getcwd, mkdir
-from os.path import dirname, exists
+from os.path import basename, exists, splitext
 from os.path import join as pj
 from shutil import rmtree
 from subprocess import call
 import sys
+from time import time
 
 
 zip_name = sys.argv[1]
-venv_dir = dirname(zip_name)
+venv_dir = splitext(basename(zip_name))[0]
 
 ####################################################################
 #
 print "deploy oaserver"
 #
 ####################################################################
+t0 = time()
 call(["unzip", zip_name])
 
 # relocate venv
@@ -23,17 +25,18 @@ execfile("relocate.py")
 chdir(cwd)
 
 # start virtual env
-act_script = pj(venv_dir, "Scripts", "activate_this.py")
+act_script = pj(venv_dir, "bin", "activate_this.py")
 execfile(act_script, dict(__file__=act_script))
 
+t1 = time()
 ####################################################################
 #
 print "use oaserver"
 #
 ####################################################################
-execfile("perform computation.py")
+execfile("job.py")
 
-
+t2 = time()
 ####################################################################
 #
 print "clean"
@@ -41,3 +44,16 @@ print "clean"
 ####################################################################
 if exists(venv_dir):
     rmtree(venv_dir)
+
+t3 = time()
+####################################################################
+#
+print "time"
+#
+####################################################################
+print "deploy", t1 - t0
+print "compute", t2 - t1
+print "clean", t3 - t2
+
+
+
