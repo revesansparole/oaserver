b147140d8eb3955da34e0829964d7712753094ef
revesansparole <revesansparole@gmail.com>
2016-01-25 10:54:37 +0100

    small example to launch oas

---
+++ example/launch_rest_server.py
@@ -0,0 +1,14 @@
+from oaserver.oa_server_rest import OAServerRest
+
+####################################################################
+#
+#   launch oaserver
+#
+####################################################################
+address = "193.49.108.1"
+address = "10.0.14.242"
+# address = "127.0.0.1"
+port = 6543
+
+oas = OAServerRest("doofus", address, port)
+oas.run()
